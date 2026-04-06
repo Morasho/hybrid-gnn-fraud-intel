c435a23dfcf68699cbfd4bf65bc9146523562782
IMBEKA <imbeka06@gmail.com>
2026-04-06 19:50:23 +0300

    created the settings page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Transactions from './pages/Transaction';
 import FraudNetwork from './pages/FraudNetwork'; 
 import Alerts from './pages/Alerts';
 import Reports from './pages/Reports';
+import Settings from './pages/Settings';
 
 
 
@@ -13,12 +14,12 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} /> {/* <-- Updated route */}
+          <Route path="/" element={<Home />} /> {/*  Updated route */}
           <Route path="/transactions" element={<Transactions />} />
           <Route path="/network" element={<FraudNetwork />} />
           <Route path="/alerts" element={<Alerts />} />
           <Route path="/reports" element={<Reports />} />
-
+          <Route path="/settings" element={<Settings />} />
           <Route path="*" element={<div>Page under construction</div>} />        </Routes>
       </Layout>
     </Router>

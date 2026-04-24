721b6e85cb6a7e8f8e448430a9d361b4c72483c4
IMBEKA <imbeka06@gmail.com>
2026-04-24 23:36:44 +0300

    Implemented SampleDataProvider into app root

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AlertsProvider } from './context/AlertsContext';
+import { SampleDataProvider } from './context/SampleDataContext';
 import Layout from './components/Layout';
 import Home from './pages/Home'; // t
 import Transactions from './pages/Transaction';
@@ -15,20 +16,22 @@ import Settings from './pages/Settings';
 function App() {
   return (
     <AlertsProvider>
-       <Router>
+      <SampleDataProvider>
+        <Router>
           <Layout>
-             <Routes>
-                <Route path="/" element={<Home />} /> {/*  Updated route */}
-                <Route path="/transactions" element={<Transactions />} />
-                <Route path="/network" element={<FraudNetwork />} />
-                <Route path="/alerts" element={<Alerts />} />
-                <Route path="/models" element={<Models />} />
-                <Route path="/ai-bot" element={<AIBot />} />
-                <Route path="/reports" element={<Reports />} />
-                <Route path="/settings" element={<Settings />} />
-              </Routes>
+            <Routes>
+              <Route path="/" element={<Home />} /> {/*  Updated route */}
+              <Route path="/transactions" element={<Transactions />} />
+              <Route path="/network" element={<FraudNetwork />} />
+              <Route path="/alerts" element={<Alerts />} />
+              <Route path="/models" element={<Models />} />
+              <Route path="/ai-bot" element={<AIBot />} />
+              <Route path="/reports" element={<Reports />} />
+              <Route path="/settings" element={<Settings />} />
+            </Routes>
           </Layout>
         </Router>
+      </SampleDataProvider>
     </AlertsProvider>
   );
 }
